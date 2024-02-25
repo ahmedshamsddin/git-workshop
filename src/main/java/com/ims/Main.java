42d6e5c2fb769e9434e509febebedce4ca8108fa
adnanfaleh <adnanfaleh@stu.aydin.edu.tr>
2024-02-25 15:55:55 +0300

    ya niggas

--- src/main/java/com/ims/Main.java
+++ src/main/java/com/ims/Main.java
@@ -3,6 +3,6 @@ package com.ims;
 public class Main {
     public static void main(String[] args) {
         App.main(args);
-        // testing commit Nigga
+        // testing commit Niggas
     }
 }
