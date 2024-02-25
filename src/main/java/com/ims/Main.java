3add70e12200bfe2b61119565b8919cd614d9c44
zmaswad05 <zmaswad05@gmail.com>
2024-02-25 16:37:21 +0300

    i added this comment

--- src/main/java/com/ims/Main.java
+++ src/main/java/com/ims/Main.java
@@ -2,8 +2,8 @@ package com.ims;
 
 public class Main {
     public static void main(String[] args) {
-        App.main(args);
-        // testing commit Niggas
-        // Hello
+
+        App.main(args);     
+        // TEST 1
     }
 }
