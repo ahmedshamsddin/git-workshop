8ca1df334a13ead7806d404f77b4c4927447edb2
Mujahidio <135789106+Mujahidio@users.noreply.github.com>
2024-03-08 19:59:34 +0300

    Update Main.java
    test

--- src/main/java/com/ims/Main.java
+++ src/main/java/com/ims/Main.java
@@ -4,7 +4,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+//hgfvkh,bjk,
 public class Main {
 
     public static void main(String[] args) {
